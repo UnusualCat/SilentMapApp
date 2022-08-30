afe0dfc3ccca38f0de33821eb0cb089e364561cf
davidFisso <davide.donadio@protonmail.com>
2022-08-30 18:16:42 +0200

    Aggiunta classe GeofenceSettings di tipo Serializable.

---
+++ app/src/main/java/com/example/silentmapapp/GeofenceSettings.kt
@@ -0,0 +1,65 @@
+package com.example.silentmapapp
+
+import android.content.Context
+import android.media.AudioManager
+import androidx.core.content.ContextCompat
+import java.io.Serializable
+
+data class GeofenceSettings(val geofenceID: String, val latitudine: Double, val longitudine: Double, val raggio: String, val colore: Int, val silentMode: Boolean): Serializable {
+
+    fun EnableFavoriteMode(context: Context)
+    {
+        audioManager = ContextCompat.getSystemService(
+            context,
+            AudioManager::class.java
+        ) as AudioManager
+
+        if (silentMode)
+        {
+            audioManager.adjustStreamVolume(AudioManager.STREAM_RING , AudioManager.ADJUST_MUTE , 0)
+            sendGeofenceMuteNotification(context)
+        } else
+        {
+            audioManager.adjustStreamVolume(
+                AudioManager.STREAM_RING ,
+                AudioManager.ADJUST_UNMUTE ,
+                0
+            )
+            sendGeofenceUnmuteNotification(context)
+        }
+    }
+
+    fun DisableFavoriteMode(context: Context)
+    {
+        audioManager = ContextCompat.getSystemService(
+            context,
+            AudioManager::class.java
+        ) as AudioManager
+
+        if (!silentMode)
+        {
+            audioManager.adjustStreamVolume(AudioManager.STREAM_RING , AudioManager.ADJUST_MUTE , 0)
+            sendGeofenceMuteNotification(context)
+        } else
+        {
+            audioManager.adjustStreamVolume(
+                AudioManager.STREAM_RING ,
+                AudioManager.ADJUST_UNMUTE ,
+                0
+            )
+            sendGeofenceUnmuteNotification(context)
+        }
+    }
+
+    override fun toString(): String {
+
+        var stringa = "Nome: '$geofenceID'\nRaggio: '$raggio'm\nModalitá: "
+
+        stringa += if(silentMode)
+            "Silenzioso"
+        else
+            "Suoneria"
+
+        return stringa
+    }
+}
